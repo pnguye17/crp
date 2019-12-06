5b2dcff763b8e4c3e603d1246dd41d12a2a4e45c
Peter Nguyen <peter.nguyen17@gmail.com>
2019-12-06 01:51:32 -0500

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Nav from "react-bootstrap/Nav"
+
+class Navbar extends Component {
+    render() {
+     return (
+        <Nav>
+
+            <li> Hello</li>
+        </Nav>
+
+
+        )
+     }
+     
+}
+
+
+export default Navbar
