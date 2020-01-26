29cae6b23a78516b94752c0e6980c27fbf2fe12d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-26 15:30:33 +0000

    Deploy pnguye17/crp to github.com/pnguye17/crp.git:gh-pages

---
+++ precache-manifest.22d97560a964ff2996c8963a914418cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176e3da40c7c61abe299c328710b6ecf",
+    "url": "/crp/index.html"
+  },
+  {
+    "revision": "f0d0c0dfefe8b1ea937f",
+    "url": "/crp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "fcc1520c06fb46244584",
+    "url": "/crp/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "f0d0c0dfefe8b1ea937f",
+    "url": "/crp/static/js/2.2904f537.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/crp/static/js/2.2904f537.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fcc1520c06fb46244584",
+    "url": "/crp/static/js/main.4d70f210.chunk.js"
+  },
+  {
+    "revision": "9f42134c4da6fec8baa3",
+    "url": "/crp/static/js/runtime-main.6a0487fc.js"
+  }
+]);
